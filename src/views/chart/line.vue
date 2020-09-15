bd4d6663c57098b809532d18d188200e630b58d2
jjiahuang <jjiahuang@xlcwnet.com>
2020-09-15 20:01:27 +0800

    提交折线图

--- src/views/chart/line.vue
+++ src/views/chart/line.vue
@@ -1,79 +1,104 @@
 <template>
-  <div class="app-container">
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row
-    >
-      <el-table-column align="center" label="ID" width="95">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+  <div class="line">
+    <chart :option="lineOption" />
   </div>
 </template>
-
 <script>
-import { getList } from '@/api/table'
-
-export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
+  import chart from '@/components/chart/index.vue'
+  export default {
+    name: 'line',
+    components: {
+      chart
+    },
+    data() {
+      return {
+        chartData: {
+          expectedData: [100, 120, 161, 134, 105, 160, 165],
+          actualData: [120, 82, 91, 154, 162, 140, 145]
+        },
+        lineOption: {
+          xAxis: {
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            boundaryGap: false, // 坐标轴两边是否留白  
+          },
+          grid: {
+            left: 10,
+            right: 10,
+            bottom: 20,
+            containLabel: true // grid 区域是否包含坐标轴的刻度标签
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross'
+            },
+            padding: [5, 10]
+          },
+          yAxis: {
+            axisTick: {
+              show: false
+            }
+          },
+          legend: {
+            data: ['expected', 'actual']
+          },
+          series: [{
+              name: 'expected',
+              itemStyle: {
+                normal: {
+                  color: '#FF005A',
+                  lineStyle: {
+                    color: '#FF005A',
+                    width: 2
+                  }
+                }
+              },
+              smooth: true,
+              type: 'line',
+              data: [],
+              animationDuration: 2800,
+              animationEasing: 'cubicInOut'
+            },
+            {
+              name: 'actual',
+              smooth: true,
+              type: 'line',
+              itemStyle: {
+                normal: {
+                  color: '#3888fa',
+                  lineStyle: {
+                    color: '#3888fa',
+                    width: 2
+                  },
+                  areaStyle: {
+                    color: '#f3f8ff'
+                  }
+                }
+              },
+              data: [],
+              animationDuration: 2800,
+              animationEasing: 'quadraticOut'
+            }
+          ]
+        }
       }
-      return statusMap[status]
-    }
-  },
-  data() {
-    return {
-      list: null,
-      listLoading: true
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData() {
-      this.listLoading = true
-      getList().then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
+    },
+    mounted(){
+      this.goData()
+    },
+    methods: {
+      goData(){
+        this.lineOption.series[0].data = this.chartData.expectedData
+        this.lineOption.series[1].data = this.chartData.actualData
+      }
     }
   }
-}
+
 </script>
+<style lang="scss" scoped>
+  .line {
+    background: #fff;
+    padding: 16px 5px 0 5px;
+  }
+
+</style>
