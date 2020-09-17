8d508b3b0bf246e2f99c011454dad55c2b76f97d
jjiahuang <jjiahuang@xlcwnet.com>
2020-09-17 10:09:21 +0800

    提交柱状图

--- src/views/chart/bar.vue
+++ src/views/chart/bar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="bar">
     <chart :option="barOption" width="50%" height="600px" style="float:left" />
-    <chart :option="animationOption" width="50%" style="float:left" />
-    <!-- <chart :option="markerOption" width="50%" style="float:left;margin-top:50px" /> -->
+    <chart :option="animationOption" width="50%" height="600px" style="float:left" />
+    <chart :option="datasetOption" width="60%" height="400px" style="float:left;margin-top:-50px" />
+    <chart :option="stackOption" width="60%" height="400px" style="float:left;margin-top:30px" />
   </div>
 </template>
 <script>
@@ -132,15 +133,11 @@
               magicType: {
                 type: ['stack', 'tiled']
               },
-              dataView: {},
-              saveAsImage: {
-                pixelRatio: 2
-              }
             }
           },
           tooltip: {},
           xAxis: {
-            data: this.xAxisData,
+            data: [],
             splitLine: {
               show: false
             }
@@ -149,7 +146,7 @@
           series: [{
             name: 'bar',
             type: 'bar',
-            data: this.animationData1,
+            data: [],
             animationDelay: function (idx) {
               return idx * 10;
             }
@@ -165,6 +162,128 @@
           animationDelayUpdate: function (idx) {
             return idx * 5;
           }
+        },
+        datasetOption: {
+          dataset: { //数据集
+            source: [
+              ['score', 'amount', 'product'],
+              [89.3, 58212, 'Matcha Latte'],
+              [57.1, 78254, 'Milk Tea'],
+              [74.4, 41032, 'Cheese Cocoa'],
+              [50.1, 12755, 'Cheese Brownie'],
+              [89.7, 20145, 'Matcha Cocoa'],
+              [68.1, 79146, 'Tea'],
+              [19.6, 91852, 'Orange Juice'],
+              [10.6, 101852, 'Lemon Juice'],
+              [32.7, 20112, 'Walnut Brownie']
+            ]
+          },
+          grid: {
+            left: '30',
+            containLabel: true
+          },
+          xAxis: {
+            name: 'amount'
+          },
+          yAxis: {
+            type: 'category'
+          },
+          visualMap: {
+            orient: 'horizontal', // 垂直放置
+            left: 'center',
+            bottom:'0',
+            min: 10,
+            max: 100,
+            text: ['High Score', 'Low Score'],
+            dimension: 0, // 数据的『哪个维度』
+            inRange: {
+              color: ['#D7DA8B', '#E15457']
+            }
+          },
+          series: [{
+            type: 'bar',
+            encode: { // 解码
+              // Map the "amount" column to X axis.
+              x: 'amount',
+              // Map the "product" column to Y axis
+              y: 'product'
+            }
+          }]
+        },
+        stackOption: {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: { // 坐标轴指示器，坐标轴触发有效
+              type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          legend: {
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: {
+            type: 'value'
+          },
+          yAxis: {
+            type: 'category',
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          },
+          series: [{
+              name: '直接访问',
+              type: 'bar',
+              stack: '总量',// 堆叠效果
+              label: {// 数值大小显示
+                show: true,
+                position: 'insideRight'
+              },
+              data: [320, 302, 301, 334, 390, 330, 320]
+            },
+            {
+              name: '邮件营销',
+              type: 'bar',
+              stack: '总量',
+              label: {
+                show: true,
+                position: 'insideRight'
+              },
+              data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+              name: '联盟广告',
+              type: 'bar',
+              stack: '总量',
+              label: {
+                show: true,
+                position: 'insideRight'
+              },
+              data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+              name: '视频广告',
+              type: 'bar',
+              stack: '总量',
+              label: {
+                show: true,
+                position: 'insideRight'
+              },
+              data: [150, 212, 201, 154, 190, 330, 410]
+            },
+            {
+              name: '搜索引擎',
+              type: 'bar',
+              stack: '总量',
+              label: {
+                show: true,
+                position: 'insideRight'
+              },
+              data: [820, 832, 901, 934, 1290, 1330, 1320]
+            }
+          ]
         }
       }
     },
@@ -178,6 +297,9 @@
           this.animationData1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
           this.animationData2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
         }
+        this.animationOption.xAxis.data = this.xAxisData
+        this.animationOption.series[0].data = this.animationData1
+        this.animationOption.series[1].data = this.animationData2
       }
     }
   }
@@ -187,7 +309,7 @@
   .bar {
     background: #fff;
     padding: 16px 5px 0 5px;
-    min-height: 800px;
+    min-height: 1400px;
   }
 
 </style>
