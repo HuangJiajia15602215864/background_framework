02af332960629efbb4b6c6137f2f3c95f34c1d65
jjiahuang <jjiahuang@xlcwnet.com>
2020-09-29 14:58:17 +0800

    全局过滤器

--- src/views/plug/i18n.vue
+++ src/views/plug/i18n.vue
@@ -5,15 +5,30 @@
     <p>{{'*'|repeat(3)}}</p>
     <p>{{'starta'|replace('a','**')}}</p>
     <p>{{'15602215864'|secretPhone}}</p>
-    
+    <p>{{new Date()|formatDate}}</p>
+    <p>{{3|change(list,'pid','qq')}}</p>
   </div>
 </template>
 
 <script>
+  export default {
+    name: 'i18n',
+    data() {
+      return {
+        list: [{
+          pid: 1,
+          qq: '可可'
+        },{
+          pid: 2,
+          qq: '佳儿'
+        },{
+          pid: 3,
+          qq: '佳。'
+        },]
+      }
+    }
+  }
 
-export default {
-  name: 'i18n',
-}
 </script>
 
 <style lang="scss" scoped>
